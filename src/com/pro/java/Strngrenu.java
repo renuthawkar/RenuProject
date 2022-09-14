2a10ef5cdd8e2adabe3c2621b8c8a663e8bd0cc2
renuthawkar <renu.thawkar2612@gmail.com>
2022-09-14 14:56:50 +0530

    renu project

---
+++ src/com/pro/java/Strngrenu.java
@@ -0,0 +1,15 @@
+package com.pro.java;
+
+public class Strngrenu {
+	  public static void main(String[] args) {
+			
+	
+	String s="ABC";
+	String s1="ABC";
+	
+		
+	System.out.println(s.equals(s1));
+
+   
+	}
+}
---
+++ src/com/pro/java/immutable.java
@@ -0,0 +1,36 @@
+package com.pro.java;
+
+final class renu {
+	private final int i;
+
+	renu(int i) {
+
+		this.i = i;
+
+	}
+
+	public renu modify(int i) {
+		if (this.i == i) {
+
+			return this;
+		}
+		else {
+			return new renu(i);
+		}
+		 }
+	}
+
+	public class immutable {
+		  public static void main(String[] args) {
+			   
+			  renu r=new renu(10);
+			  renu r1=r.modify(10);
+			  renu r3=r1.modify(10);
+			//  renu r2=r3.modify(100);
+			  
+		  System.out.println(r==r1);
+		  System.out.println(r==r3);
+		 // System.out.println(r==r2);
+		  }	
+		
+	}
